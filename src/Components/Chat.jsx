8787465d76c28aa1fae54e1b914554be09a087a3
Jess <jess2410@hotmail.fr>
2022-10-07 10:14:38 +0200

    final commit

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Cam from './../assets/images/cam.png'
 import Add from './../assets/images/add.png'
 import More from './../assets/images/more.png'
 import Messages from './Messages'
 import Input from './Input'
+import { ChatContext } from '../context/ChatContext'
 
 
 export default function Chat() {
+
+  const { data } = useContext(ChatContext);
+
   return (
     <div className='chat'>
       <div className="chatInfo">
-        <span>Jane</span>
+        <span>{data.user.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt="icon" />
           <img src={Add} alt="icon" />
--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-export default function Message() {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className='message owner'>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="messageInfo">
-          <img src="https://images.pexels.com/photos/5794559/pexels-photo-5794559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="photo avatar" />
-          <span>Just now</span>
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>hello</p>
-        <img src="https://images.pexels.com/photos/5794559/pexels-photo-5794559.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="photo avatar" />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Message;
